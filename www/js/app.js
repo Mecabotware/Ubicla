5685bf21d7d29bf2430b365a88e3662e08f93594
Bul Ikana <bul.ikana@gmail.com>
2014-03-18 23:39:52 -0600

    Added AngularJS directive for Leaflet. Also added left menu.

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,12 @@
-angular.module('ubicla', ['ionic'])
+angular.module('Ubicla', ['ionic', 'leaflet-directive'])
+
+.controller('mapCtrl', function($scope) {
+	// var map = L.map('map', {
+	//     center: [19.043925, -98.198204],
+	//     zoom: 13,
+	//     zoomControl: false,
+	// });
+	// L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	//     maxZoom: 18
+	// }).addTo(map);
+});
